557041d77badc54e33c5bf65617d369272101ff3
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-07-16 10:18:53 +0530

    tsT

---
+++ class-14-EventDOM/color.js
@@ -0,0 +1,12 @@
+let changeColor1=()=>{
+   /*  let tag_Ref=document.getElementById('btn1')
+    console.log(tag_Ref) */
+    //document.getElementById('btn1').innerHTML="GM"
+    document.getElementById('btn1').style.backgroundColor="blue"
+    
+}
+let changeColor2=()=>{
+   /*  let tag_Ref = document.getElementsByTagName('button')[1]
+    console.log(tag_Ref) */
+    document.getElementsByTagName('button')[1].style.backgroundColor="yellow"
+}
---
+++ class-14-EventDOM/color1.js
@@ -0,0 +1,5 @@
+let changeColor1=()=>{
+    console.log("Test Case 123");
+    alert("Test Case 123")
+    
+}
